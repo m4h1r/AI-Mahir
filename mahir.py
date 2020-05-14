a6f522a2482a1f8c9b237e639a1dc43ef929b843
= <mahiryildizhan.com@gmail.com>
2020-05-14 20:29:01 +0100

    Getting name with console and greatings.

--- mahir.py
+++ mahir.py
@@ -1,1 +1,6 @@
-print("Hello, please write your name.")
+import io
+import sys
+print("Hello, wellcome to my AI project Mahir.")
+print("This project aims to create an AI assistant named Mahir.")
+user = input("Please tell me your name=")
+print("Hello " +user+ ". Hope you are doing well.")
